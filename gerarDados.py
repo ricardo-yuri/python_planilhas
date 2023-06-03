1e75acc5d38dd7c373b5096704533b2ae2f92366
Yuri <ricardo-yuri@outlook.com>
2023-06-03 14:45:20 -0300

    Adicionado tela com arquivo de origem e destino para melhor visualização

--- gerarDados.py
+++ gerarDados.py
@@ -1,49 +1,86 @@
 from tkinter import filedialog, messagebox
 from openpyxl.utils.dataframe import dataframe_to_rows
 
+import tkinter as tk
 import openpyxl
 import pandas as pd
 
-messagebox.showinfo(title='Introducao', message='Selecione primeiramente o arquivo de origem e depois o arquivo de destino ')
 
-path_origem = filedialog.askopenfilenames()
-path_origem = path_origem[0]
-path_destino = filedialog.askopenfilenames()
-path_destino = path_destino[0]
+def selecionar_arquivo_origem():
+    global path_origem;
+    path_origem = filedialog.askopenfilename()
+    lbl_arquivo_origem.config(text="Arquivo de origem: " + str(path_origem))
 
-try:
-    df1 = pd.read_excel(path_origem)
-    df2 = pd.read_excel(path_destino, sheet_name='TESTE')
-except Exception as e:
-    messagebox.showerror(title='ERROR', message='Erro ao ler os arquivos Excel: ' + str(e))
 
-try:
-    id_a_serem_procurados = df2['CONTRATO'].values
-except Exception as e:
-    messagebox.showerror(title='ERROR', message='Erro ao buscar id a serem consultados ' + str(e))
+def selecionar_arquivo_destino():
+    global path_destino;
+    path_destino = filedialog.askopenfilename()
+    lbl_arquivo_destino.config(text="Arquivo de destino: " + str(path_destino))
 
-dados_filtrados = []
 
-for id in id_a_serem_procurados:
+def cancelar():
+    janela.destroy()
+
+
+def gerar():
     try:
-        filtro = df1['TRATO_EBT_NET'] == id
-        dados_filtrados.append(df1.loc[filtro])
+        df1 = pd.read_excel(path_origem)
+        df2 = pd.read_excel(path_destino, sheet_name='TESTE')
     except Exception as e:
-        messagebox.showerror(title='ERROR', message='Erro ao buscar dados ' + str(e))
+        messagebox.showerror(title='ERROR', message='Erro ao ler os arquivos Excel: ' + str(e))
 
-messagebox.showinfo(title='Geracao de Dados', message='Iniciando processo de gravação dos dados')
-df = pd.concat(dados_filtrados)
+    try:
+        id_a_serem_procurados = df2['CONTRATO'].values
+    except Exception as e:
+        messagebox.showerror(title='ERROR', message='Erro ao buscar id a serem consultados ' + str(e))
 
-try:
-    workbook = openpyxl.load_workbook(path_destino)
+    dados_filtrados = []
 
-    worksheet = workbook.create_sheet('RELATORIO', index=0)
+    for id in id_a_serem_procurados:
+        try:
+            filtro = df1['TRATO_EBT_NET'] == id
+            dados_filtrados.append(df1.loc[filtro])
+        except Exception as e:
+            messagebox.showerror(title='ERROR', message='Erro ao buscar dados ' + str(e))
 
-    for row in dataframe_to_rows(df, index=False, header=True):
-        worksheet.append(row)
+    messagebox.showinfo(title='Geracao de Dados', message='Iniciando processo de gravação dos dados')
+    df = pd.concat(dados_filtrados)
 
-    workbook.save(path_destino)
+    try:
+        workbook = openpyxl.load_workbook(path_destino)
 
-    messagebox.showinfo(title='SUCESSO!', message='Dados gerados com sucesso!!')
-except Exception as e:
-    messagebox.showerror(title='ERROR', message='Erro ao salvar dados na planilha ' + str(e))
+        worksheet = workbook.create_sheet('RELATORIO', index=0)
+
+        for row in dataframe_to_rows(df, index=False, header=True):
+            worksheet.append(row)
+
+        workbook.save(path_destino)
+
+        messagebox.showinfo(title='SUCESSO!', message='Dados gerados com sucesso!!')
+    except Exception as e:
+        messagebox.showerror(title='ERROR', message='Erro ao salvar dados na planilha ' + str(e))
+
+    janela.destroy()
+
+
+janela = tk.Tk()
+
+btn_origem = tk.Button(janela, text="Selecionar arquivo de origem", command=selecionar_arquivo_origem)
+btn_origem.grid(row=0, column=0)
+
+lbl_arquivo_origem = tk.Label(janela, text="Arquivo de origem:")
+lbl_arquivo_origem.grid(row=0, column=1)
+
+btn_destino = tk.Button(janela, text="Selecionar arquivo de destino", command=selecionar_arquivo_destino)
+btn_destino.grid(row=1, column=0)
+
+lbl_arquivo_destino = tk.Label(janela, text="Arquivo de destino:")
+lbl_arquivo_destino.grid(row=1, column=1)
+
+btn_gerar = tk.Button(janela, text="Gerar", command=gerar)
+btn_gerar.grid(row=2, column=0)
+
+btn_cancelar = tk.Button(janela, text="Cancelar", command=cancelar)
+btn_cancelar.grid(row=2, column=1)
+
+janela.mainloop()
